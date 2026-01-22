f40a4749228b4697b52310e09223db5b90774b81
Awab Tanveer <awab.tanveer@logicielservice.com>
2026-01-22 11:56:07 +0500

    Test case name Change

---
+++ UI-Automation/tests/loginDashboard.spec.js
@@ -0,0 +1,39 @@
+import { expect, test } from '@playwright/test';
+import { LoginPage } from '../Pages/LoginPage.js';
+import { users } from '../utils/testdata.js';
+import { DashboardPage } from '../Pages/DashboardPage.js';
+
+test.describe('Login and Dashboard TestCase', () => {
+
+  test('User login and verify dashboard page workflow', async ({ page }) => {
+    test.setTimeout(120000); 
+    const loginPage = new LoginPage(page);
+    const dashboardPage = new DashboardPage(page);
+     // Set viewport to full screen
+    await page.setViewportSize({ width: 1900, height: 945 });
+
+    // Login and verify successful login
+    await loginPage.navigate();
+    await loginPage.login(users.username, users.password);
+
+    await page.waitForURL(/\/combined-contracts;id=/, { timeout: 30000 });
+    await expect(page).toHaveURL(/\/combined-contracts;id=/);
+
+    await dashboardPage.searchAndApply('AAPL');
+
+    await dashboardPage.doubleClickSymbol('AAPL');
+
+    await dashboardPage.expandFirstGroup();
+
+    await dashboardPage.clickSymbolByIndex('AAPL', 3);
+
+    await dashboardPage.doubleClickSymbol('AAPL');
+    await dashboardPage.verifyBrokerDetailsHeader();
+
+    await dashboardPage.expandFirstGroup();
+
+    await dashboardPage.clickSymbolByIndex('AAPL', 3);
+    await dashboardPage.verifyBrokerDetailsHeader();
+  });
+
+});
